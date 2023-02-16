4ccef5835c695bf23776951914d8054238ab8f3b
Michael Dao <mike@turing.edu>
2023-02-16 09:48:12 -0700

    OAuth Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "home#index"
+
+  get "/auth/github/callback", to: "sessions#create"
+  get "/dashboard", to: "dashboard#show"
 end
